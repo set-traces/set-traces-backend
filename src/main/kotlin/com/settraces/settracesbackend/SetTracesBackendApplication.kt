8c2c94b6854db7adc751d19fbe024770f12ef5b0
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-03-23 17:06:29 +0100

    start.spring.io initial commit

---
+++ src/main/kotlin/com/settraces/settracesbackend/SetTracesBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.settraces.settracesbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SetTracesBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SetTracesBackendApplication>(*args)
+}
