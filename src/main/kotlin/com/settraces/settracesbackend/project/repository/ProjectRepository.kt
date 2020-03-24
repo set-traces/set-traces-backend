48b6a5805f3b8947865dad06ad937d5f69a5d7c3
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-03-24 01:53:19 +0100

    added repos

--- src/main/kotlin/com/settraces/settracesbackend/project/repository/ProjectRepository.kt
+++ src/main/kotlin/com/settraces/settracesbackend/project/repository/ProjectRepository.kt
@@ -1,4 +1,12 @@
 package com.settraces.settracesbackend.project.repository
 
-class ProjectRepository {
+import com.settraces.settracesbackend.project.models.Project
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface ProjectRepository : JpaRepository<Project?, Long?> {
+    override fun findAll(): MutableList<Project?>
+    fun findAllById(id: UUID): Project
 }
