83767bf7ef0a94ecfbce187d1d0a10217a95acdd
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-03-24 01:54:40 +0100

    added project model

--- src/main/kotlin/com/settraces/settracesbackend/project/models/Project.kt
+++ src/main/kotlin/com/settraces/settracesbackend/project/models/Project.kt
@@ -1,4 +1,25 @@
 package com.settraces.settracesbackend.project.models
 
+import org.hibernate.annotations.Type
+import java.util.*
+import javax.persistence.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+import kotlin.collections.HashSet
+
+
+@Entity
+@Table(name = "projects", uniqueConstraints = [UniqueConstraint(columnNames = ["id"])])
 class Project {
+    @Id val id: UUID = UUID.randomUUID()
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    var name: @NotBlank @Size(max=50) String? = null
+    @Type(type="text")
+    var description: @NotBlank String? = null
+
+    constructor() {}
+    constructor(name: String, description: String):this() {
+        this.name = name;
+        this.description = description;
+    }
 }
