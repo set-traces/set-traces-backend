6e4a7bf3bcb24477b957d15047f947e985dccbc6
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-03-26 00:21:33 +0100

    added desc to playing roles

---
+++ src/main/kotlin/com/settraces/settracesbackend/database/migrations/0005-addingDescriptionToPlayingRoles.sql
@@ -0,0 +1,5 @@
+
+
+-- Adding description to playing_roles
+
+ALTER TABLE playing_roles ADD COLUMN description VARCHAR(300);
---
+++ src/main/kotlin/com/settraces/settracesbackend/project/mappers/RoleMetaWrapper.kt
@@ -0,0 +1,11 @@
+package com.settraces.settracesbackend.project.mappers
+
+import com.settraces.settracesbackend.project.models.RoleMeta
+import org.springframework.jdbc.core.RowMapper
+import java.sql.ResultSet
+
+class RoleMetaWrapper : RowMapper<RoleMeta> {
+    override fun mapRow(rs: ResultSet, rowNum: Int): RoleMeta? {
+        return RoleMeta(rs.getString("role"), rs.getString("description"), rs.getString("actor"))
+    }
+}
